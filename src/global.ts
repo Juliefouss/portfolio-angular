24d625c594a8ed2ec0118b43416cf1b2f697343b
juliefouss <foussy89@hotmail.com>
2021-06-08 13:29:44 +0200

    derniers correctifs

--- src/global.ts
+++ src/global.ts
@@ -2,6 +2,6 @@
 // on utilise le global.ts pour créer le server et ne pas devoir se repeter
 // et changer juste le lien ici et l'appeler à chaque fois que nous en avons besoin
 
-export const SERVER_LOCATION = 'http://localhost:8000/' ;
+export const SERVER_LOCATION = 'http://localhost:8001/' ;
 
 
